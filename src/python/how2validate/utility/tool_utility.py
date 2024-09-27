cdf5de348275c6e7f7dcfaa1ed40f5dedb571f4d
VigneshKna <vigneshkanna>
2024-09-27 16:52:38 +0530

    docs(cli):updated cli help summary

--- src/python/how2validate/utility/tool_utility.py
+++ src/python/how2validate/utility/tool_utility.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import sys
 
+from tabulate import tabulate
+
 from how2validate.utility.config_utility import get_package_name
 from how2validate.utility.log_utility import setup_logging
 
@@ -42,6 +44,43 @@ def get_secretservices(file_path = file_path):
                 enabled_secrets_services.append(f"{token_info['display_name']}")
     
     return enabled_secrets_services
+
+def get_secretscope(file_path = file_path):
+    """
+    Reads a JSON file containing provider tokens and logs a table of enabled services.
+
+    Args:
+        file_path (str): The path to the JSON file containing provider tokens. 
+                         Defaults to the global variable `file_path`.
+
+    Raises:
+        FileNotFoundError: If the specified file cannot be found or read.
+    
+    Returns:
+        None: This function logs a formatted table of enabled provider services.
+              If no enabled services are found, it logs a relevant message.
+    """
+    # Open and load the JSON file
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    
+    scoped_services = []
+
+    # Iterate over each provider and its tokens
+    for provider, tokens in data.items():
+        for token_info in tokens:
+            # Check if the token is enabled
+            if token_info['is_enabled']:
+                # Add the provider and service (token display name) to the scoped services list
+                scoped_services.append([provider, token_info['display_name']])
+
+    # Log the result as a table
+    if scoped_services:
+        # Log the table using the tabulate function with fancy formatting
+        logging.info(tabulate(scoped_services, headers=['Provider', 'Service'], tablefmt='fancy_outline'))
+    else:
+        # Log a message if no enabled services are found
+        logging.info("No enabled services found.")
 
 def format_serviceprovider(file_path = file_path):
     """Format service choices as a bullet-point list."""
